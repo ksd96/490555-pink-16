c2b1f1c5f8b0adaf19e4532bf625dfd46ea94404
ksd96 <45177616+ksd96@users.noreply.github.com>
2019-05-07 18:49:42 +0300

    предзащита

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,10 @@ var navMain = document.querySelector(".nav");
 var bodyMain = document.querySelector(".body");
 var navToggle = document.querySelector(".nav__toggle");
 
+var form  = document.querySelector("form");
+var error = document.querySelector(".main-form__invalid");
+var surname = document.querySelector("[name=surname]");
+var name = document.querySelector("[name=name]");
 
 bodyMain.classList.remove("body--nojs");
 
@@ -12,5 +16,16 @@ navToggle.addEventListener("click", function() {
   } else {
     bodyMain.classList.add("body--closed");
     bodyMain.classList.remove("body--opened");
+  }
+});
+
+form.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  if (!surname.value || !name.value) {
+    console.log("Нужно ввести имя, фамилию и почту");
+    error.classList.remove("main-form__invalid--invisible");
+    error.classList.add("main-form__invalid--visible");
+  } else {
+    this.submit()
   }
 });
