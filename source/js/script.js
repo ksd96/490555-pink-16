d8b894062a4b2ccbeddafcb6e07461ae1771b8c9
ksd96 <45177616+ksd96@users.noreply.github.com>
2019-05-19 13:46:31 +0300

    закончил верстку

--- source/js/script.js
+++ source/js/script.js
@@ -4,8 +4,11 @@ var navToggle = document.querySelector(".nav__toggle");
 
 var form  = document.querySelector("form");
 var error = document.querySelector(".main-form__invalid");
+var ok = document.querySelector(".main-form__valid");
 var surname = document.querySelector("[name=surname]");
 var name = document.querySelector("[name=name]");
+var buttonError = document.querySelector(".main-form__invalid-button");
+var buttonOk = document.querySelector(".main-form__valid-button");
 
 bodyMain.classList.remove("body--nojs");
 
@@ -21,11 +24,27 @@ navToggle.addEventListener("click", function() {
 
 form.addEventListener("submit", function (evt) {
   evt.preventDefault();
-  if (!surname.value || !name.value) {
+  if (!surname.value) {
     console.log("Нужно ввести имя, фамилию и почту");
     error.classList.remove("main-form__invalid--invisible");
     error.classList.add("main-form__invalid--visible");
   } else {
-    this.submit()
+    ok.classList.remove("main-form__valid--invisible");
+    ok.classList.add("main-form__valid--visible");
+    evt.preventDefault();
+  }
+});
+
+buttonError.addEventListener("click", function() {
+  if (error.classList.contains("main-form__invalid--visible")) {
+    error.classList.remove("main-form__invalid--visible");
+    error.classList.add("main-form__invalid--invisible");
+  }
+});
+
+buttonOk.addEventListener("click", function() {
+  if (ok.classList.contains("main-form__valid--visible")) {
+    ok.classList.remove("main-form__valid--visible");
+    ok.classList.add("main-form__valid--invisible");
   }
 });
